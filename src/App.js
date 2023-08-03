6ea588e5fc7cbc4a71b27118d1aa7b7cc3fbbc1a
ajayraj1111 <121021527+ajayraj1111@users.noreply.github.com>
2023-08-03 12:36:35 +0530

    created two buttons and changed its values according to the components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./Components/About";
+import Profile from "./Components/Profile";
+import { useState } from "react";
 
 function App() {
+const [state,setState] = useState('')
+let component;
+if(state === 'about'){
+  component =   <About></About>
+}
+
+if(state === 'profile'){
+  component = <Profile></Profile>
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setState('about')}  >About</button>
+      <button onClick={() => setState('profile')} >Profile</button>
+    {component}
+      
     </div>
   );
 }
