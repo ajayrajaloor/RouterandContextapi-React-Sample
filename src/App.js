4dfde7016b8e0620be76e2e8e0015d33c8e3c92c
ajayraj1111 <121021527+ajayraj1111@users.noreply.github.com>
2023-08-04 10:24:53 +0530

    route in buttons done by using useNavigate

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import About from "./Components/About";
 import Profile from "./Components/Profile";
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes,Link } from "react-router-dom"; //Route to define the endpoints & Link is used to get the routes without reloading
+import {  Route, Routes,Link, useNavigate} from "react-router-dom"; //Route to define the endpoints & Link is used to get the routes without reloading & we use useNavigate to navigate routes through buttons
+//  for doing with the buttons we need to import BrowserRouter in the index.js... for that we are going to remove from here to there   ..4..
+
 
 function App() {
   const [state, setState] = useState("");
+  const navigate = useNavigate()
 
   // let component;
   // if(state === 'about') {
@@ -25,20 +28,27 @@ function App() {
 
       {/* {component} */} {/* same as the above if condition reason      ..1.. */}
 
-      <Router>
-      <Link to='/about'>About</Link>
+      {/* <Router>        by moving the BrowserRouter to the index.js we don't need to use Router here anymore. it is there in index.js where the App is wrapped up in the Router    ..4.. */}
+      {/* <Link to='/about'>About</Link>
       <br />
-      <Link to='/profile'>Profile</Link>
+      <Link to='/profile'>Profile</Link>     ..3..  */}
+
+
+      <button onClick={() => navigate('/about')} >About</button>
+      <button onClick={() => navigate('/profile')} >Profile</button>
+
         <Routes> {/* it is a container which is a must use container to wrap the all Route  */}
           <Route Component={About} path='/about' />
           {/* here we first try to get the About component while typing the path as endpoint write inside the Route... we can also write in another way */}
           <Route Component={Profile} path="/profile"/> 
-          
+          <Route render/>
         </Routes>
-      </Router>
+
+
+      {/* </Router>     ..4..*/}
     </div>
   );
-  
+
 }
 
 export default App;
