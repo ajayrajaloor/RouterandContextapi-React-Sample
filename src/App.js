07fcfee67dcde1790f48d05c06d7982ef3e790d7
ajayraj1111 <121021527+ajayraj1111@users.noreply.github.com>
2023-08-04 09:41:37 +0530

    the reloading fixed by using the Link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import About from "./Components/About";
 import Profile from "./Components/Profile";
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; //Route to define the endpoints
+import { BrowserRouter as Router, Route, Routes,Link } from "react-router-dom"; //Route to define the endpoints & Link is used to get the routes without reloading
 
 function App() {
   const [state, setState] = useState("");
@@ -13,22 +13,32 @@ function App() {
   // }
   // if(state === 'profile'){
   //   component = <Profile></Profile>
-  // }   //we don't need to write it anymore coz we started using the React Router
+  // }   //we don't need to write it anymore coz we started using the React Router  ..1..
 
   return (
     <div className="App">
-      <button onClick={() => setState('about')}>About</button>
-      <button onClick={() => setState('profile')}>Profile</button>
-      {/* {component} */} {/* same as the above if condition reason */}
+
+   
+
+      {/* <button onClick={() => setState('about')}>About</button>
+      <button onClick={() => setState('profile')}>Profile</button>    ..2.. */}
+
+      {/* {component} */} {/* same as the above if condition reason      ..1.. */}
+
       <Router>
+      <Link to='/about'>About</Link>
+      <br />
+      <Link to='/profile'>Profile</Link>
         <Routes> {/* it is a container which is a must use container to wrap the all Route  */}
           <Route Component={About} path='/about' />
           {/* here we first try to get the About component while typing the path as endpoint write inside the Route... we can also write in another way */}
-          <Route Component={Profile} path="/profile"/>
+          <Route Component={Profile} path="/profile"/> 
+          
         </Routes>
       </Router>
     </div>
   );
+  
 }
 
 export default App;
